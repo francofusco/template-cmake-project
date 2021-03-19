7a5f668188d28aabd5490bd42655f5fbea6d37f9
Franco Fusco on Rasperry Pi4 <francofusco93@gmail.com>
2021-03-19 09:23:25 +0100

    Restore CI action

--- .github/workflows/install-gtest.sh
+++ .github/workflows/install-gtest.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 set -x
 
-echo "TEMP: not installing GTest to make sure that CI fails!"
-# cd ..
-# git clone https://github.com/google/googletest.git
-# mkdir -p googletest/build
-# cd googletest/build
-# cmake ..
-# cmake --build .
-# sudo cmake --install .
+cd ..
+git clone https://github.com/google/googletest.git
+mkdir -p googletest/build
+cd googletest/build
+cmake ..
+cmake --build .
+sudo cmake --install .
