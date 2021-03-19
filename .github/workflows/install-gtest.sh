880ce7bfced0f583cb5d8e8a01ef9b7e18bb2e47
Franco Fusco on Rasperry Pi4 <francofusco93@gmail.com>
2021-03-19 09:20:44 +0100

    Checking CI failure when missing GTest

--- .github/workflows/install-gtest.sh
+++ .github/workflows/install-gtest.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -x
 
-cd ..
-git clone https://github.com/google/googletest.git
-mkdir -p googletest/build
-cd googletest/build
-cmake ..
-cmake --build .
-sudo cmake --install .
+echo "TEMP: not installing GTest to make sure that CI fails!"
+# cd ..
+# git clone https://github.com/google/googletest.git
+# mkdir -p googletest/build
+# cd googletest/build
+# cmake ..
+# cmake --build .
+# sudo cmake --install .
